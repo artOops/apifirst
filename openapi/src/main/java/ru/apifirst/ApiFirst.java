c381c72d0f4631c8d1084f9dc5a04d673e8c3302
artOops <sir.tema2014@yandex.ru>
2024-10-07 00:09:15 +0500

    add example openapi

---
+++ openapi/src/main/java/ru/apifirst/ApiFirst.java
@@ -0,0 +1,12 @@
+package ru.apifirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFirst {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFirst.class, args);
+    }
+}
